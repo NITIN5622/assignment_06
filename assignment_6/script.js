95138cf657f32feedcd9c4d7847d8db331478a37
NITIN5622 <nitinyadav9034317060@gmail.com>
2024-10-13 23:23:30 +0530

    Add files via upload

---
+++ assignment_6/script.js
@@ -0,0 +1,21 @@
+const menuIcon=document.querySelector("#menu-icon")
+const navlinks=document.querySelector(".nav-links")
+menuIcon.onClick=()=>{
+    navlinks.classList.toggle('active')
+}
+let weatherLink=document.querySelector(".weather-link")
+weatherLink.addEventListener("click",()=>{
+    window.open("https://github.com/sonamaniAjay/web_dev_project_tic_tac_toe")
+})
+let tictactoeLink=document.querySelector(".tic-tac-toe-link")
+tictactoeLink.addEventListener('click',()=>{
+    window.open("https://github.com/sonamaniAjay/web_dev_project_tic_tac_toe")
+})
+// let visitGithub_1=document.querySelector(".visit-btn")
+// visitGithub_1.addEventListener('click',()=>{
+//     window.open("https://github.com/sonamaniAjay")
+// })
+// let visitGithub_2=document.querySelector(".fa-github")
+// visitGithub_2.addEventListener('click',()=>{
+//     window.open("https://github.com/sonamaniAjay")
+// })
